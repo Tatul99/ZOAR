f04691f54a9ef7a1de6e2995a1e6a3a77c8e967a
tatul <tatulkhachatryanait@gmail.com>
2022-11-24 13:38:49 +0400

    packages-component is ready

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,11 +2,13 @@
 <div class="Home-main">
     <menu-component />
     <building-image-component />
+    <packages-component />
 </div>
    
 </template>
 <script setup>
 import MenuComponent from "./menuComponent.vue"
 import BuildingImageComponent from './building-image.component.vue'
+import PackagesComponent from './packages-component.vue'
 
 </script>
---
+++ src/components/packages-component.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="packages-main pt-[100px] flex flex-col items-center ">
+        <div class="packages-title text-blue text-[40px] font-bold leading-[49px] mb-[40px]">ՓԱԹԵԹՆԵՐ</div>
+        <div class="under-title text-blue text-[16px] mb-[80px]">Ստորև կարող եք ընտրել ներքին հարդարման Ձեզ առավել հարմար փաթեթը։</div>
+        <div class="packages-contain pl-[250px] pr-[200px] w-[100%] ">
+                <div class=" flex justify-between cursor-pointer flex-wrap w-[100%] ">
+                    <div class="item hover-plus  relative flex flex-col items-center p-[64px] mb-[40px]" v-for="(item,i) in packageItems" :key="item.id"
+                        :class="{
+                            ' w-[30%]':i<packageItems.length-1,
+                            'border-r-[3px] border-blue':i<2,
+                            'w-[100%] flex justify-center border-b-2 border-blue':i === packageItems.length-1,
+                           
+                        }"
+                    >
+                        <div class="item-title flex justify-center leading-[39px] text-blue text-[32px] relative">{{item.title}}</div>
+                        <ul class="text-blue mt-[50px] flex flex-col"><li v-for="list in item.text" :key="list" class="text-center leading-[30px]">{{list}}</li></ul>
+                    </div>
+                </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+let packageItems = [
+    {id:1,title:'Ստանդարտ',text:['Մուտքի դուռ','Գաջած պատեր','Հոսնքի լարանցում','Դրսի պատուհաններ', 'Առաջնային ստյաժկա', 'Մուտքային ջրագծերի  հիմնական կետեր', 'Կոյուղու հիմնական կետեր']},
+    {id:2,title:'Կոմֆորտ',text:['Քիվեր Վերանորոգում', 'Պատուհանագոգ',' Ներկած   պատեր', 'Հոսանքի միացման վերջնական մոնտաժ առանց անջատիչների', 'Սանհանգույցի սալիկապատում', 'Լամինատե և պռեսգրանիտե հատակ', ' Առաստաղի գիպսակարտոն և ներկում']},
+    {id:3,title:'Կոմֆորտ  պլյուս', text:['Ջահը', 'Լույսերը', 'Ծորակները', 'Վարդակները', 'Վարագույրի կախիչները',' Խոհանոցի լամինատե կահույքը', 'Ավելանում է սանհանգույցի կահույքը']},
+    {id:4,title:'Լյուքս',text:['Վերոնշյալ տարբերակներից որևէ մեկին ավելանում է անհատական ինտերիեր դիզայն և դրա վերանորոգման աշխատանքները']}
+]
+</script>
+
+<style scoped>
+.hover-plus:before{
+content:'';
+position:absolute;
+width:32px;
+height: 2px;
+background: #002366;
+top:15vh;
+right: 50%;
+}
+.hover-plus:hover:before{
+    display: none;
+}
+.hover-plus:hover:after{
+content:'+';
+position:absolute;
+font-size: 52px;
+color: #002366;
+top:10vh;
+left: 50%;
+}
+.hover-plus:hover .item-title{
+    font-weight: 700;
+}
+
+</style>
